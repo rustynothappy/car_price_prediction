3313a98e5890bc0e4f702b3fe9aace7f18d2bd6b
rustynothappy <91842257+rustynothappy@users.noreply.github.com>
2021-11-23 22:38:54 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ import sklearn
 from sklearn.preprocessing import StandardScaler
 app = Flask(__name__)
 model = pickle.load(open('/home/achilles/Downloads/random_forest_regression_model.pkl', 'rb'))
-
 @app.route('/',methods=['GET'])
 def Home():
     return render_template('index.html')
@@ -52,11 +51,3 @@ def predict():
 
 if __name__=="__main__":
     app.run(debug=True)
-
-
-
-
-
-
-
-
